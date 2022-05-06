35d97c44c8b2d2efed80a9f71c8f73f72146a9d2
Jorge Loredo <jorgel@teamventi.com>
2022-05-05 20:00:16 -0500

    missing routes added, cors import added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import projectRoutes from './routes/projectRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
 
 const app = express();
 app.use(express.json());
@@ -13,6 +16,8 @@ connectDB();
 
 // Routing
 app.use('/api/users', userRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 4000;
 
